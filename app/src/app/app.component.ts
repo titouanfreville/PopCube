8148f47594b8ca6fcb4be595141c031cd36ea538
ceylass <213759@supinfo.com>
2017-05-06 19:30:19 +0000

    [WIP] Profil on nav

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 import { ApiService } from './shared';
 
+import { User } from '../model/user';
+
+import { UserService } from '../service/user';
+
 import '../style/app.scss';
 
 const remote = require('electron').remote;
@@ -14,12 +18,19 @@ const remote = require('electron').remote;
   selector: 'my-app', // <my-app></my-app>
   template: require('./app.component.html'),
   styles: [require('./app.component.scss')],
+  providers: [UserService]
 })
 export class AppComponent {
   url = 'https://github.com/preboot/angular2-webpack';
   public img = 'img/tab.svg';
 
-  constructor(private api: ApiService) {
+  currentUser;
+
+  constructor(
+    private api: ApiService,
+    private _user: UserService
+    ) {
+    this.currentUser = this._user.retrieveUser();
   }
 
   minimize() {
